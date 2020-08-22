f15e81bf4b32fde42c864ba73fa022ed82663505
John Fisher <jjfisher91@gmail.com>
2020-08-22 12:29:27 -0500

    created user and root routes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const root_routes = require('./routes/root-routes')
 
 const app = express()
 
 app.use(cors({ origin: 'http://localhost:3000' }))
-
-app.get('/', (req, res) => {
-	const users = [
-		'John',
-		'Max',
-		'Pul',
-		'Khalil'
-	]
-	res.send(users)
-})
+app.use(root_routes)
 
 app.listen(8000, () => {
 	console.log('fnm-js listening on port 8000!')
